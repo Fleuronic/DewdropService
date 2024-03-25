f7ac1d4f9acd67a8adb31edc7bd5acd269346eca
Jordan Kay <jordan@fleuronic.com>
2024-03-25 17:38:12 -0400

    Rearrange specs

--- Sources/DewdropService/Specs/HighlightSpec.swift
+++ Sources/DewdropService/Specs/HighlightSpec.swift
@@ -1,13 +1,10 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
-import struct Dewdrop.Raindrop
 import struct Dewdrop.Highlight
 import struct Dewdrop.Collection
 
 public protocol HighlightSpec {
 	associatedtype HighlightListResult
-	associatedtype RaindropHighlightsResult
 
 	func listHighlights(inCollectionWith id: Collection.ID?) async -> HighlightListResult
-	func fetchRaindropHighlights(with id: Raindrop.ID) async -> RaindropHighlightsResult
 }
--- Sources/DewdropService/Specs/RaindropSpec.swift
+++ Sources/DewdropService/Specs/RaindropSpec.swift
@@ -4,6 +4,8 @@ import struct Dewdrop.Raindrop
 
 public protocol RaindropSpec {
 	associatedtype RaindropDetailsResult
+	associatedtype RaindropHighlightsResult
 
 	func fetchRaindropDetails(with id: Raindrop.ID) async -> RaindropDetailsResult
+	func fetchRaindropHighlights(with id: Raindrop.ID) async -> RaindropHighlightsResult
 }
