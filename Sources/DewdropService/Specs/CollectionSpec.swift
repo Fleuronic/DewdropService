02b369e339d8f5d8046ebba80880e69c1fa798a0
Jordan Kay <jordan@fleuronic.com>
2024-04-01 09:14:37 -0400

    Support covers

--- Sources/DewdropService/Specs/CollectionSpec.swift
+++ Sources/DewdropService/Specs/CollectionSpec.swift
@@ -6,9 +6,12 @@ public protocol CollectionSpec {
 	associatedtype CollectionListResult
 	associatedtype CollectionDetailsResult
 	associatedtype CollaboratorListResult
+	associatedtype CoverListResult
 
 	func listRootCollections() async -> CollectionListResult
 	func listChildCollections() async -> CollectionListResult
 	func fetchCollectionDetails(with id: Collection.ID) async -> CollectionDetailsResult
 	func listCollaborators(ofCollectionWith id: Collection.ID) async -> CollaboratorListResult
+	func searchForCovers(with text: String) async -> CoverListResult
+	func listFeaturedCovers() async -> CoverListResult
 }
