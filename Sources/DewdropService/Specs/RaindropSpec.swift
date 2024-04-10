4aab2adf424771960383093fd9c6d100ba81b2a4
Jordan Kay <jordan@fleuronic.com>
2024-04-10 17:15:41 -0400

    Update spec

--- Sources/DewdropService/Specs/RaindropSpec.swift
+++ Sources/DewdropService/Specs/RaindropSpec.swift
@@ -1,16 +1,17 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
 import struct Dewdrop.Raindrop
+import struct Dewdrop.Collection
 
 public protocol RaindropSpec {
-//	associatedtype RaindropListResult
+	associatedtype RaindropListResult
 	associatedtype RaindropDetailsResult
 	associatedtype RaindropHighlightsResult
 	associatedtype RaindropSuggestionsResult
 	associatedtype RaindropCopyDownloadResult
 	associatedtype RaindropRemovalResult
 
-	func listRaindrops() async -> RaindropListResult
+	func listRaindrops(inCollectionWith id: Collection.ID, searchingFor search: String?, sortedBy sort: Raindrop.Sort?, onPage page: Int?, listing raindropsPerPage: Int?) async -> RaindropListResult
 	func fetchRaindropDetails(with id: Raindrop.ID) async -> RaindropDetailsResult
 	func fetchRaindropHighlights(with id: Raindrop.ID) async -> RaindropHighlightsResult
 	func listSuggestions(forRaindropWith id: Raindrop.ID) async -> RaindropSuggestionsResult
