b89b72365d0942140f0bdc1bfb4e0fd6ba44a03b
Jordan Kay <jordan@fleuronic.com>
2024-04-25 17:31:07 -0400

    Update user spec

--- Sources/DewdropService/Specs/UserSpec.swift
+++ Sources/DewdropService/Specs/UserSpec.swift
@@ -1,18 +1,27 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
 import struct Dewdrop.User
+import struct Dewdrop.Group
+import struct Dewdrop.Collection
+import struct Dewdrop.Network
 
 public protocol UserSpec {
 	associatedtype UserDetailsResult
 	associatedtype UserAuthenticatedDetailsResult
+	associatedtype NetworkConnectionResult
+	associatedtype NetworkDisconnectionResult
 
 	func fetchUserAuthenticatedDetails() async -> UserAuthenticatedDetailsResult
 	func fetchUserDetails(with id: User.ID) async -> UserDetailsResult
+	func connectSocialNetworkAccount(from provider: Network.Provider) async -> NetworkConnectionResult
+	func disconnectSocialNetworkAccount(from provider: Network.Provider) async -> NetworkDisconnectionResult
 	
 	func updateUser(
 		email: String?,
 		fullName: String?,
 		oldPassword: String?,
-		newPassword: String?
+		newPassword: String?,
+		config: User.Config?,
+		groups: [(Group, [Collection.ID])]?
 	) async -> UserAuthenticatedDetailsResult
 }
