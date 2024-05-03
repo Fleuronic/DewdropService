9f2d4656e2c833f2685f696c858d5da6f0ddf4ad
Jordan Kay <jordan@fleuronic.com>
2024-05-02 22:08:40 -0400

    Add remaining raindrop import fields properties

--- Sources/DewdropService/Fields/Raindrop/RaindropImportFields.swift
+++ Sources/DewdropService/Fields/Raindrop/RaindropImportFields.swift
@@ -2,7 +2,20 @@
 
 import InitMacro
 
+import struct Dewdrop.Media
+import struct Foundation.URL
+import struct Foundation.Date
+
 @Init public struct RaindropImportFields {
+	public let url: URL
+	public let title: String
+	public let excerpt: String?
+	public let coverURL: URL?
+	public let media: [Media]
+	public let note: String?
+	public let isFavorite: Bool
+	public let creationDate: Date
+	public let updateDate: Date
 	public let tags: [TagNameFields]
 	public let highlights: [HighlightImportFields]?
 }
