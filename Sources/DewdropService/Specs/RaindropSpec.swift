0e251ef4808b539ebdb652f2ac0a86fe2f2a3594
Jordan Kay <jordan@fleuronic.com>
2024-05-06 11:05:35 -0500

    Update spec

--- Sources/DewdropService/Specs/RaindropSpec.swift
+++ Sources/DewdropService/Specs/RaindropSpec.swift
@@ -5,19 +5,19 @@ import struct Dewdrop.Collection
 import struct Foundation.URL
 
 public protocol RaindropSpec {
-	associatedtype RaindropListResult
 	associatedtype RaindropDetailsResult
 	associatedtype RaindropHighlightsResult
-	associatedtype RaindropSuggestionsResult
 	associatedtype RaindropCopyDownloadResult
+	associatedtype RaindropSuggestionsResult
+	associatedtype RaindropListResult
 	associatedtype RaindropRemovalResult
 
-	func listRaindrops(inCollectionWith id: Collection.ID, searchingFor search: String?, sortedBy sort: Raindrop.Sort?, onPage page: Int?, listing raindropsPerPage: Int?) async -> RaindropListResult
-	func fetchRaindropDetails(with id: Raindrop.ID) async -> RaindropDetailsResult
-	func fetchRaindropHighlights(with id: Raindrop.ID) async -> RaindropHighlightsResult
+	func fetchDetails(forRaindropWith id: Raindrop.ID) async -> RaindropDetailsResult
+	func fetchHighlights(forRaindropWith id: Raindrop.ID) async -> RaindropHighlightsResult
+	func downloadPermanentCopy(ofRaindropWith id: Raindrop.ID) async -> RaindropCopyDownloadResult
 	func listSuggestions(forRaindropWith id: Raindrop.ID) async -> RaindropSuggestionsResult
 	func listSuggestionsForNewRaindrop(with url: URL) async -> RaindropSuggestionsResult
-	func downloadPermanentCopy(ofRaindropWith id: Raindrop.ID) async -> RaindropCopyDownloadResult
+	func listRaindrops(inCollectionWith id: Collection.ID, searchingFor search: String?, sortedBy sort: Raindrop.Sort?, onPage page: Int?, listing raindropsPerPage: Int?) async -> RaindropListResult
 	func removeRaindrop(with id: Raindrop.ID) async -> RaindropRemovalResult
 	func removeRaindrops(fromCollectionWith id: Collection.ID?, matching ids: [Raindrop.ID]?, searchingFor search: String?) async -> RaindropRemovalResult
 }
