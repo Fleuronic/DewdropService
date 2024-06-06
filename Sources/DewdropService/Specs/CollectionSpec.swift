adee0f220cdef199e088dcb988640b0d903c720c
Jordan Kay <jordan@fleuronic.com>
2024-06-06 18:36:45 -0400

    Update specs

--- Sources/DewdropService/Specs/CollectionSpec.swift
+++ Sources/DewdropService/Specs/CollectionSpec.swift
@@ -7,62 +7,62 @@ import struct Foundation.URL
 
 public protocol CollectionSpec {
 	associatedtype CollectionListResult
-	associatedtype SystemCollectionListResult
-	associatedtype CollaboratorListResult
-	associatedtype CoverListResult
-	associatedtype CollectionDetailsResult
-	associatedtype CollectionCoverUploadResult
-	associatedtype CollectionShareResult
-	associatedtype CollectionJoinResult
-	associatedtype CollectionMergeResult
-	associatedtype CollectionReorderResult
-	associatedtype CollectionExpansionCollapseResult
-	associatedtype CollaboratorUpdateResult
-	associatedtype CollaboratorDeletionResult
-	associatedtype CollectionRemovalResult
-	associatedtype CollectionUnshareLeaveResult
-	associatedtype EmptyCollectionsRemovalResult
+	// associatedtype SystemCollectionListResult
+	// associatedtype CollaboratorListResult
+	// associatedtype CoverListResult
+	// associatedtype CollectionDetailsResult
+	// associatedtype CollectionCoverUploadResult
+	// associatedtype CollectionShareResult
+	// associatedtype CollectionJoinResult
+	// associatedtype CollectionMergeResult
+	// associatedtype CollectionReorderResult
+	// associatedtype CollectionExpansionCollapseResult
+	// associatedtype CollaboratorUpdateResult
+	// associatedtype CollaboratorDeletionResult
+	// associatedtype CollectionRemovalResult
+	// associatedtype CollectionUnshareLeaveResult
+	// associatedtype EmptyCollectionsRemovalResult
 	
-	func createCollection(
-		title: String?,
-		coverURL: URL?,
-		public: Bool?,
-		view: Collection.View?,
-		sortIndex: Int?,
-		parentID: Collection.ID?
-	) async -> CollectionDetailsResult
+	// 	func createCollection(
+	// 		title: String?,
+	// 		coverURL: URL?,
+	// 		public: Bool?,
+	// 		view: Collection.View?,
+	// 		sortIndex: Int?,
+	// 		parentID: Collection.ID?
+	// 	) async -> CollectionDetailsResult
+	// 
+	// 	func updateCollection(
+	// 		with id: Collection.ID,
+	// 		title: String?,
+	// 		coverURL: URL?,
+	// 		public: Bool?,
+	// 		view: Collection.View?,
+	// 		sortIndex: Int?,
+	// 		expanded: Bool?,
+	// 		parentID: Collection.ID?
+	// 	) async -> CollectionDetailsResult
 
-	func updateCollection(
-		with id: Collection.ID,
-		title: String?,
-		coverURL: URL?,
-		public: Bool?,
-		view: Collection.View?,
-		sortIndex: Int?,
-		expanded: Bool?,
-		parentID: Collection.ID?
-	) async -> CollectionDetailsResult
-
-	func fetchDetails(forCollectionWith id: Collection.ID) async -> CollectionDetailsResult
-	func listCollaborators(ofCollectionWith id: Collection.ID) async -> CollaboratorListResult
+	// func fetchDetails(forCollectionWith id: Collection.ID) async -> CollectionDetailsResult
+	// func listCollaborators(ofCollectionWith id: Collection.ID) async -> CollaboratorListResult
 	func listRootCollections() async -> CollectionListResult
 	func listChildCollections() async -> CollectionListResult
-	func listSystemCollections() async -> SystemCollectionListResult
-	func expandCollections(_ expanded: Bool) async -> CollectionExpansionCollapseResult
-	func reorderCollections(by sort: Collection.Sort) async -> CollectionReorderResult
-	func mergeCollections(with ids: [Collection.ID], intoCollectionWith id: Collection.ID) async -> CollectionMergeResult
-	func removeCollection(with id: Collection.ID) async -> CollectionRemovalResult
-	func removeCollections(with ids: [Collection.ID]) async -> CollectionRemovalResult
-	func removeEmptyCollections() async -> EmptyCollectionsRemovalResult
-	func emptyTrash() async -> CollectionRemovalResult
-
-	func shareCollection(with id: Collection.ID, toUsersWithEmails emails: [String], as role: Collaborator.Role) async -> CollectionShareResult
-	func acceptInvitation(toJoinCollectionWith id: Collection.ID, viaEmailWithToken token: String) async -> CollectionJoinResult
-	func changeAccessLevelOfCollaborator(with id: User.ID, inCollectionWith collectionID: Collection.ID, to role: Collaborator.Role) async -> CollaboratorUpdateResult
-	func removeCollaborator(with id: User.ID, fromCollectionWith collectionID: Collection.ID) async -> CollaboratorDeletionResult
-	func unshareLeaveCollection(with id: Collection.ID) async -> CollectionUnshareLeaveResult
-
-	func listFeaturedCovers() async -> CoverListResult
-	func searchForCovers(with text: String) async -> CoverListResult
-	func uploadCover(forCollectionWith id: Collection.ID, usingFileAt url: URL, withName filename: String) async -> CollectionCoverUploadResult
+	// 	func listSystemCollections() async -> SystemCollectionListResult
+	// 	func expandCollections(_ expanded: Bool) async -> CollectionExpansionCollapseResult
+	// 	func reorderCollections(by sort: Collection.Sort) async -> CollectionReorderResult
+	// 	func mergeCollections(with ids: [Collection.ID], intoCollectionWith id: Collection.ID) async -> CollectionMergeResult
+	// 	func removeCollection(with id: Collection.ID) async -> CollectionRemovalResult
+	// 	func removeCollections(with ids: [Collection.ID]) async -> CollectionRemovalResult
+	// 	func removeEmptyCollections() async -> EmptyCollectionsRemovalResult
+	// 	func emptyTrash() async -> CollectionRemovalResult
+	// 
+	// 	func shareCollection(with id: Collection.ID, toUsersWithEmails emails: [String], as role: Collaborator.Role) async -> CollectionShareResult
+	// 	func acceptInvitation(toJoinCollectionWith id: Collection.ID, viaEmailWithToken token: String) async -> CollectionJoinResult
+	// 	func changeAccessLevelOfCollaborator(with id: User.ID, inCollectionWith collectionID: Collection.ID, to role: Collaborator.Role) async -> CollaboratorUpdateResult
+	// 	func removeCollaborator(with id: User.ID, fromCollectionWith collectionID: Collection.ID) async -> CollaboratorDeletionResult
+	// 	func unshareLeaveCollection(with id: Collection.ID) async -> CollectionUnshareLeaveResult
+	// 
+	// 	func listFeaturedCovers() async -> CoverListResult
+	// 	func searchForCovers(with text: String) async -> CoverListResult
+	// 	func uploadCover(forCollectionWith id: Collection.ID, usingFileAt url: URL, withName filename: String) async -> CollectionCoverUploadResult
 }
