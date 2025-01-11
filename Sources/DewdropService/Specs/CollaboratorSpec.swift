3d4530d552087d29c7851cd64e51569d42c4c27f
Jordan Kay <jordan@fleuronic.com>
2025-01-11 12:25:19 -0500

    Update collaborator spec

--- Sources/DewdropService/Specs/CollaboratorSpec.swift
+++ Sources/DewdropService/Specs/CollaboratorSpec.swift
@@ -8,9 +8,13 @@ import protocol Catena.Scoped
 public protocol CollaboratorSpec {
 	associatedtype CollaboratorList: Scoped<CollaboratorListFields>
 	associatedtype CollaboratorRoleChange
+	associatedtype CollaboratorInvitation
+	associatedtype CollaboratorAcceptance
 
 	associatedtype CollaboratorListFields: CollaboratorFields
 
 	func listCollaborators(ofCollectionWith id: Collection.ID) async -> CollaboratorList
 	func changeRole(ofCollaboratorWith id: Collaborator.ID, inCollectionWith collectionID: Collection.ID, to role: Collaborator.Role) async -> CollaboratorRoleChange
+	func shareCollection(with id: Collection.ID, toCollaborateAs role: Collaborator.Role, sendingTo emails: [String]) async -> CollaboratorInvitation
+	func acceptInvitation(forCollectionWith id: Collection.ID, usingToken token: String) async -> CollaboratorAcceptance
 }
