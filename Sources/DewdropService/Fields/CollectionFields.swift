8dc1141abcf0bb15d9c11dff23dc1b8636628169
Jordan Kay <jordan@fleuronic.com>
2025-01-30 12:06:59 -0500

    Add impossible fields

--- Sources/DewdropService/Fields/CollectionFields.swift
+++ Sources/DewdropService/Fields/CollectionFields.swift
@@ -1,9 +1,13 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
 import struct Dewdrop.Collection
+import struct Catena.ImpossibleFields
 import protocol Catena.Valued
 import protocol Catena.Fields
 
 public protocol CollectionFields: Fields {
 	associatedtype Model: Valued<Collection> = Collection.Identified
 }
+
+// MARK: -
+extension ImpossibleFields: CollectionFields where Model == Collection {}
--- Sources/DewdropService/Fields/RaindropFields.swift
+++ Sources/DewdropService/Fields/RaindropFields.swift
@@ -1,9 +1,13 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
 import struct Dewdrop.Raindrop
+import struct Catena.ImpossibleFields
 import protocol Catena.Valued
 import protocol Catena.Fields
 
 public protocol RaindropFields: Fields {
 	associatedtype Model: Valued<Raindrop> = Raindrop.Identified
 }
+
+// MARK: -
+extension ImpossibleFields: RaindropFields where Model == Raindrop {}
